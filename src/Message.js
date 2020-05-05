fb5679f8e938ea7715c834a48ae4464439c058e5
David Trussler <mail@davidtrussler.net>
2020-05-05 08:24:09 +0100

    Understanding reconciliation

--- src/Message.js
+++ src/Message.js
@@ -2,6 +2,31 @@ import React, {Component} from 'react';
 import {ActionButton} from './ActionButton'; 
 
 export class Message extends Component {
+	constructor(props) {
+		super(props); 
+
+		this.state = {
+			showSpan: false
+		}
+	}
+
+	handleClick = (e) => {
+		this.setState({showSpan: !this.state.showSpan}); 
+		this.props.callback(e); 
+	}
+
+	getMessageElement() {
+		let div = 
+			<div
+				id="messageDiv" 
+				className="h5 text-center p-2"
+			>
+				{this.props.message}
+			</div>
+
+		return this.state.showSpan ? <span>{div}</span> : div; 
+	}
+
 	render() {
 		console.log(`Render Message Component `); 
 
@@ -10,11 +35,10 @@ export class Message extends Component {
 				<ActionButton
 					theme="primary"
 					{... this.props}
+					callback = {this.handleClick}
 				/>
 
-				<div className="h5 text-center p-2">
-					{this.props.message}
-				</div>
+				{this.getMessageElement()}
 			</div>
 		)
 	}
